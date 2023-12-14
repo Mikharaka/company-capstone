619a9d97e9cec1675b657fe907df88a3ccb9f61f
Mikharaka <jerjeremi40@gmail.com>
2023-12-15 03:21:52 +0700

    baseURL

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,7 +9,7 @@ import { Toaster } from 'react-hot-toast'
 import { UserContextProvider } from '../context/userContext'
 import Dashboard from "./pages/Dashboard"
 
-axios.defaults.baseURL = 'http://localhost:8000';
+axios.defaults.baseURL = 'http://34.128.96.83:8000';
 axios.defaults.withCredentials = true
 
 const App = () => {
